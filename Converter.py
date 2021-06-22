bee957799cf4259d2e026bf1367fd9c1ede712d6
John Grohman <grohmanjp@gmail.com>
2021-06-21 23:59:51 -0400

    Added Converter file

---
+++ Converter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+def converter_function(number, unit_to_convert_to: str):
+    '''
+    Function that can convert fahrenheit to celcius vice versa.
+    '''
+    if unit_to_convert_to == 'F':
+        number = (number * 9/5) + 32
+    elif unit_to_convert_to == 'C':
+        number = (number - 32) * (5/9)
+    else:
+        return ('Invalid Unit')
