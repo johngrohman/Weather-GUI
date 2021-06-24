c6ec909de07e06fe127f973a64075e9102be284f
John Grohman <grohmanjp@gmail.com>
2021-06-24 00:09:11 -0400

    Added toggling between F and C

--- Weather-App.py
+++ Weather-App.py
@@ -16,13 +16,14 @@ class City_Weather:
     def __init__(self, name: str, temp: int):
         self.name = name
         self.temp = temp
+        self.units = 'F'
 
-    def convert_units(self, unit: str):
-        if unit == 'F':
-            self.units = 'F'
-        else:
+    def convert_units(self):
+        if self.units == 'F':
             self.units = 'C'
-        self.temp = converter_function(self.temp, unit)
+        else:
+            self.units = 'F'
+        self.temp = converter_function(self.temp, self.units)
 
 def get_weather(city):
     weather_key = ''
@@ -30,32 +31,29 @@ def get_weather(city):
     params = {'APPID': weather_key, 'q': city, 'units': 'imperial'}
     response = requests.get(url, params = params)
     weather = response.json()
-    print(weather)
 
     if weather.get('message') == 'city not found':
         city_label = 'Unknown'
     else:
-        city_label = weather.get('name')
-
-        city = City_Weather(weather.get('name'))
-
-    print('city_label: ', city_label)
-
-
+        city = City_Weather(weather.get('name'), weather['main']['temp'])
+        convert = tk.Button(top_frame, bd = 0, text = 'F/C', bg = 'blue', command = lambda: city.convert_units())
+        convert.place(relx = 0, relwidth = .1, relheight = .9, rely = .05)
 
 root = tk.Tk()
 
 canvas = tk.Canvas(root, height = CANVAS_HEIGHT, width = CANVAS_WIDTH, bg = '#f9f7f7')
 canvas.pack()
 
-# Search Bar
-search_frame = tk.Frame(root, bg = 'red', bd = 5)
-search_frame.place(relx = 0.45, rely = 0.05, relwidth = 0.5, relheight = 0.1)
+# Top Bar
+top_frame = tk.Frame(root, bg = 'red', bd = 5)
+top_frame.place(relx = 0.05, rely = 0.05, relwidth = 0.9, relheight = 0.1)
 
-entry = tk.Entry(search_frame, font = 40, bg = 'green', bd = 0, state = 'normal')
-entry.place(relheight = 1, relwidth = .7)
+entry = tk.Entry(top_frame, font = 40, bg = 'green', bd = 0, state = 'normal')
+entry.place(relx = .5, relheight = 1, relwidth = .3)
 
-search = tk.Button(search_frame, bd = 0, text = "Search", bg = 'blue', command = lambda: get_weather(entry.get()))
-search.place(relx = .7, relwidth = .3, relheight = .9, rely = .05)
+search = tk.Button(top_frame, bd = 0, text = "Search", bg = 'blue', command = lambda: get_weather(entry.get()))
+search.place(relx = .8, relwidth = .2, relheight = .9, rely = .05)
 
-# root.mainloop()
+
+
+root.mainloop()
